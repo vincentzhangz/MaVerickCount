d1f4479e2c21855ceafd4a7840900bb5b0c8a53b
Vincent <vincentzhangz@github.com>
2020-03-28 00:03:20 +0700

    add register page and firebase

--- app/src/main/java/com/vincentzhangz/maverickcount/RegisterActivity.kt
+++ app/src/main/java/com/vincentzhangz/maverickcount/RegisterActivity.kt
@@ -3,10 +3,10 @@ package com.vincentzhangz.maverickcount
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
 }
