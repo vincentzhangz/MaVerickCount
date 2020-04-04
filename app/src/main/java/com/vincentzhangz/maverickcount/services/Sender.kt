f8023872438256ecfc368028a148760f44678f9e
octa8888 <malvin00.com@gmail.com>
2020-04-04 20:24:50 +0700

    notif cancel

--- app/src/main/java/com/vincentzhangz/maverickcount/services/Sender.kt
+++
@@ -1,9 +0,0 @@
-package com.vincentzhangz.maverickcount.services
-
-class NotificationData(val user:String,val body:String,val title:String,val sented:String){
-    constructor():this("","","","")
-}
-
-class Sender(val data:NotificationData, val to:String){
-    constructor():this(NotificationData("","","",""),"")
-}
