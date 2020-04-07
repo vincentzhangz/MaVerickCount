d678411ee886a05e12a49d2875fbb48fb4d5f6fa
octa8888 <malvin00.com@gmail.com>
2020-04-07 21:46:07 +0700

    push notif hampir complete

---
+++ app/src/main/java/com/vincentzhangz/maverickcount/models/NotificationModel.kt
@@ -0,0 +1,2 @@
+package com.vincentzhangz.maverickcount.models
+
---
+++ app/src/main/java/com/vincentzhangz/maverickcount/services/MyFirebaseMessagingService.kt
@@ -0,0 +1,6 @@
+package com.vincentzhangz.maverickcount.services;
+
+import android.app.Service;
+
+public class MyFirebaseMessaginService extends Service {
+}
