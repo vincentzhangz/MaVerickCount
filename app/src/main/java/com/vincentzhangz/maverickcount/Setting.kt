73cf852aa75278a38d05df4f84492c59b545dc9e
Vincent <vincentzhangz@outlook.com>
2020-04-09 00:37:42 +0700

    a few change chat UI

--- app/src/main/java/com/vincentzhangz/maverickcount/Setting.kt
+++ app/src/main/java/com/vincentzhangz/maverickcount/Setting.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.vincentzhangz.maverickcount.utilities.ThemeManager
 import kotlinx.android.synthetic.main.fragment_setting.view.*
 
 
 class SettingFragment : Fragment() {
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,6 +30,33 @@ class SettingFragment : Fragment() {
             themeManager.loadTheme()
             this.fragmentManager!!.beginTransaction().detach(this).attach(this).commit()
         }
+        val languages = arrayOf("English", "Indonesia")
+        val arrayAdapter = ArrayAdapter(
+            activity!!.baseContext,
+            android.R.layout.simple_spinner_dropdown_item, languages
+        )
+//        with(rootView.language) {
+//            adapter = arrayAdapter
+//        }
+
+        rootView.language.adapter = arrayAdapter
+
+//        rootView.language.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//            override fun onNothingSelected(parent: AdapterView<*>?) {
+//                TODO("Not yet implemented")
+//            }
+//
+//            override fun onItemSelected(
+//                parent: AdapterView<*>?,
+//                view: View?,
+//                position: Int,
+//                id: Long
+//            ) {
+//                TODO("Not yet implemented")
+//                SystemUtility.toast(rootView.context, "Tst")
+//            }
+//
+//        }
         return rootView
     }
 
